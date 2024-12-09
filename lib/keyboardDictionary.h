6b26718f620517fba0bd487e66f4c026170bf750
Анастасия <citadeln@mail.ru>
2024-12-09 14:10:31 +0500

    redact grammatics mistakes

--- lib/keyboardDictionary.h
+++ lib/keyboardDictionary.h
@@ -4,9 +4,9 @@
 
 #define FILE_NAME "tests/dictionary.txt"
 #define ROWS_IN_KEYBOARD \
-  5  // 3 + before + after in oreder not to be out of array
+  5  // 3 + before + after in order not to be out of array
 #define COLS_IN_KEYBOARD \
-  12  // 10 + before + after in oreder not to be out of array
+  12  // 10 + before + after in order not to be out of array
 
 bool areAdjacent(char c1, char c2);
 bool isValidChar(char x);
